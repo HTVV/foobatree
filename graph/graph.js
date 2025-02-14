bf0fe88a28ef433acec742b55cd7725026314436
HTV <hvartiainen09@gmail.com>
2025-02-14 14:52:39 +0200

    ,maybe chrome fix

--- graph/graph.js
+++ graph/graph.js
@@ -885,7 +885,7 @@ function idSetNode(uuid) {
     ${
       localStorage.getItem("show-pics") === "true"
         ? `<img style="width: 80%;  align-self: center; display: block; margin-left: auto;margin-right: auto;" src="${
-            person.pic ? person.pic : "/img/default_pic.png"
+            person.pic ? person.pic : "https://media.istockphoto.com/id/1451587807/vector/user-profile-icon-vector-avatar-or-person-icon-profile-picture-portrait-symbol-vector.jpg?s=612x612&w=0&k=20&c=yDJ4ITX1cHMh25Lt1vI1zBn2cAKKAlByHBvPJ8gEiIg="
           }"></img>`
         : ""
     }
